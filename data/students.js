1d781ba73cda0fa9b950fb012315419b8ec68bcf
Cendel Altunisik <cendelaltinisik@gmail.com>
2022-12-23 14:51:00 +0100

    initialize

---
+++ data/students.js
@@ -0,0 +1,6 @@
+export const students = [
+  { id: 1, name: "Jasmine", point: 80 },
+  { id: 2, name: "Adam", point: 32 },
+  { id: 3, name: "Sarah", point: 56 },
+  { id: 4, name: "Mark", point: 76 },
+];
